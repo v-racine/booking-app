f78955deb28e401fc3e98680c8e8c0dfcf88585c
Valerie Racine <vracine21@gmail.com>
2025-09-25 14:57:07 -0700

    feat: adding schedules

---
+++ public/front-end/booking3.js
@@ -0,0 +1,94 @@
+document.addEventListener("DOMContentLoaded", () => {
+  let scheduleContainer;
+  let btnAdd;
+  let form;
+  let staffs;
+  let scheduleCount = 0;
+
+  async function fetchStaffMembers() {
+    const response = await fetch("/api/staff_members");
+    return response.json();
+  }
+
+  // Template for creating a schedule
+  function scheduleTemplate() {
+    const staffOptions = staffs
+      .map(({ id, name }) => `<option value="${id}">${name}</option>`)
+      .join("");
+
+    return `
+    <fieldset id="schedule_${scheduleCount}">
+      <legend>Schedule ${scheduleCount}</legend>
+
+      <div>
+        <label for="staff_${scheduleCount}">Staff Name:</label>
+        <select id="staff_${scheduleCount}" name="staff_${scheduleCount}">${staffOptions}</select>
+      </div>
+
+      <div>
+        <label for="date_${scheduleCount}">Date:</label>
+        <input type="text" id="date_${scheduleCount}" name="date_${scheduleCount}" placeholder="mm-dd-yy">
+      </div>
+
+      <div>
+        <label for="time_${scheduleCount}">Time:</label>
+        <input type="text" id="time_${scheduleCount}" name="time_${scheduleCount}" placeholder="hh:mm">
+      </div>
+
+    </fieldset>`;
+  }
+
+  // Add new schedule on button click
+  function addSchedule() {
+    scheduleCount += 1;
+    const scheduleHTML = scheduleTemplate();
+    scheduleContainer.insertAdjacentHTML("beforeend", scheduleHTML);
+  }
+
+  // Convert form inputs to JSON
+  function formInputsToJson(form) {
+    const json = [];
+    for (let i = 0; i < scheduleCount; i += 1) {
+      json.push({
+        staff_id: form[`staff_${i + 1}`].value,
+        date: form[`date_${i + 1}`].value,
+        time: form[`time_${i + 1}`].value,
+      });
+    }
+    return { schedules: json };
+  }
+
+  // Handle form submission
+  async function handleSubmit(event) {
+    event.preventDefault();
+    const json = JSON.stringify(formInputsToJson(event.target));
+
+    const response = await fetch(event.target.action, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: json,
+    });
+
+    if (response.status === 201) form.reset();
+    alert(await response.text());
+  }
+
+  async function main() {
+    scheduleContainer = document.getElementById("schedules");
+    btnAdd = document.getElementById("btnAdd");
+    form = document.querySelector("form");
+
+    staffs = await fetchStaffMembers();
+
+    addSchedule();
+
+    btnAdd.addEventListener("click", (event) => {
+      event.preventDefault();
+      addSchedule();
+    });
+
+    form.addEventListener("submit", handleSubmit);
+  }
+
+  main();
+});
